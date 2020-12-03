d5fd3306ded0526a06b114b899e6f37caf0409ae
Cameron-Stock <clstock33@gmail.com>
2020-12-02 19:34:36 -0600

    created sentence_maker function

--- function.py
+++ function.py
@@ -1,3 +1,24 @@
-def foo(name):
-    return "Hi %s" % name.title()
+#def foo(name):
+    #return "Hi %s" % name.title()
     #This will make the first letter of the name uppercase 
+
+
+def sentence_maker(phrase):
+    interrogatives = ("how", "what", "why")
+    capitalize = phrase.capitalize()
+    if phrase.startswith(interrogatives):
+        return "{}?".format(capitalize)
+    else:
+        return "{}.".format(capitalize)
+
+#print(sentence_maker("how are you"))
+
+results = []
+while True:
+    user_input = input("Say something:")
+    if user_input == "\end":
+        break
+    else:
+        results.append(sentence_maker(user_input))
+
+print(" ".join(results))
